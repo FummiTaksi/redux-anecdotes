ddae5e34b4b89d1db07f6cf480f4d3048a87c281
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-01-28 19:52:03 +0200

    start notification comes from notification reducer

--- redux-anecdotes-v2/src/components/Notification.js
+++ redux-anecdotes-v2/src/components/Notification.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import notificationReducer from '../reducers/notificationReducer'
 
 class Notification extends React.Component {
   render() {
+    const message = this.props.store.getState().notification
     const style = {
       border: 'solid',
       padding: 10,
@@ -9,7 +11,7 @@ class Notification extends React.Component {
     }
     return (
       <div style={style}>
-        render here notification...
+        {message}
       </div>
     )
   }

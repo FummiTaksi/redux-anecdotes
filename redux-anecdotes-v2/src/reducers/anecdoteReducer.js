143b7465dd93024bf0fddfb1c253c446bc6c5e14
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-01-30 16:43:05 +0200

    getting anecdotes from db.json

--- redux-anecdotes-v2/src/reducers/anecdoteReducer.js
+++ redux-anecdotes-v2/src/reducers/anecdoteReducer.js
@@ -1,12 +1,3 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000*Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
@@ -17,10 +8,10 @@ const asObject = (anecdote) => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
+const initialState = []
 
 const reducer = (store = initialState, action) => {
-  if (action.type==='VOTE') {
+  if (action.type ==='VOTE') {
     const old = store.filter(a => a.id !==action.id)
     const voted = store.find(a => a.id === action.id)
 
@@ -29,6 +20,9 @@ const reducer = (store = initialState, action) => {
   if (action.type === 'CREATE') {
 
     return [...store, { content: action.content, id: getId(), votes:0 }]
+  }
+  if (action.type === 'INIT') {
+    return action.content
   }
 
   return store
@@ -45,6 +39,13 @@ export const anecdoteVote = (id) => {
   return {
     type: 'VOTE',
     id: id
+  }
+}
+
+export const anecdoteInitialization = (content) => {
+  return {
+    type: 'INIT',
+    content: content
   }
 }
 
